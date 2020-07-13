8a9d619d2f78a1c15b9553e0453d019d7d33e2bf
Md Rana Mahmud <ranamd0@gmail.com>
2020-07-13 10:11:51 +0600

    index split done

---
+++ explore-js/index-split.js
@@ -0,0 +1,12 @@
+var promise = "I promise I'll work HARD to become a programmer";
+var name = "Md Rana Mahmud";
+var country = `Bangladesh`;
+
+console.log(promise)
+
+console.log(promise.toLowerCase())
+console.log(promise.toLocaleUpperCase())
+console.log(promise.indexOf("work"))
+console.log(promise.indexOf('hard'))
+console.log(promise.split(" "))
+console.log(promise.split("I"))
