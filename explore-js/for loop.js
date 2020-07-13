b15c2692ad682750fbeed2a78c8dda21b31a7704
Md Rana Mahmud <ranamd0@gmail.com>
2020-07-13 12:46:05 +0600

    for loop done

---
+++ explore-js/for loop.js
@@ -0,0 +1,10 @@
+for(var i = 0; i <15; i++){
+    console.log(i);
+}
+
+var nums = [55, 66, 77, 88, 99, 11, 41];
+
+for(var i = 0; i < nums.length; i++){
+    var element = nums[i];
+    console.log(element);
+}
