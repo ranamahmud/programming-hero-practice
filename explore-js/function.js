6a3cfd79c549772a0da674d67e2781c78363d35e
Md Rana Mahmud <ranamd0@gmail.com>
2020-07-13 13:00:28 +0600

    Function Done

---
+++ explore-js/function.js
@@ -0,0 +1,10 @@
+function sayLoveYou(){
+    console.log("Jaan o Baby");
+    console.log("Billy baby .. koi tumi");
+}
+sayLoveYou();
+var date = 14;
+var place = "Restaurant";
+sayLoveYou();
+var hangout = "TSC";
+sayLoveYou();
