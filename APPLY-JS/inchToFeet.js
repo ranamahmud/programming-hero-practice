bfc257ec63c4eca1bf29bff88f85aa85fb8e0dc0
Md Rana Mahmud <ranamd0@gmail.com>
2020-07-14 09:57:23 +0600

    inchToFeet done

---
+++ APPLY-JS/inchToFeet.js
@@ -0,0 +1,26 @@
+var inch = 156;
+console.log(inch);
+console.log(inch);
+console.log(inch);
+var foot = inch / 12;
+console.log(foot);
+console.log(foot);
+console.log(foot);
+
+// function inch to feet
+
+function inchToFeet(inch){
+    var feet =  inch / 12;
+    return feet;
+}
+
+console.log(inch)
+
+var nanaFeet = inchToFeet(156);
+console.log(nanaFeet);
+
+var naniFeet = inchToFeet(288);
+console.log(naniFeet);
+
+var dadiFeet = inchToFeet(300);
+console.log(dadiFeet);
