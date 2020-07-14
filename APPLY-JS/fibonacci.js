5b1e340f761258da5d28ad5d4f113087ab62eae9
Md Rana Mahmud <ranamd0@gmail.com>
2020-07-15 05:58:39 +0600

    fibonacci function added

--- APPLY-JS/fibonacci.js
+++ APPLY-JS/fibonacci.js
@@ -11,3 +11,13 @@ for(var i = 2; i<= 12; i++){
 }
 console.log(fibo);
 
+function fibonacci(n){
+    var fibo = [0, 1];
+    for(var i = 2; i<= n; i++){
+        fibo[i] = fibo[i - 2] + fibo[i - 1];
+    }
+    return fibo;
+}
+var result = fibonacci(12);
+console.log(result);
+
