bb30b275ce398d7f5576bc6406fd2b8f50f0411f
Md Rana Mahmud <ranamd0@gmail.com>
2020-07-15 07:07:23 +0600

    random done

---
+++ javascript problems/random.js
@@ -0,0 +1,19 @@
+var num = 2.5;
+var result = Math.floor(num);
+var result2 = Math.ceil(num);
+var result3 = Math.round(num);
+
+
+var randomNumber = Math.random() * 6;
+var output = Math.round(randomNumber);
+console.log(result);
+console.log(result2);
+console.log(result3);
+console.log(output);
+
+for (var i = 0; i <= 10; i++) {
+    var randomNumber = Math.random() * 6;
+    var output = Math.round(randomNumber);
+    console.log(output);
+
+}
