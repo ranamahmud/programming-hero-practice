cddf53853788f40bb41d7f88cfd9055924facfa3
Md Rana Mahmud <ranamd0@gmail.com>
2020-07-18 02:53:59 +0600

    revisit 16.6.1 done

---
+++ bonus-js/bonus.js
@@ -0,0 +1,17 @@
+let number = 59;
+number = 79;
+number = 119;
+const name = 'Rana';
+console.log(number);
+console.log(name);
+
+var marks = [89, 97, 89, 68, 94];
+
+for(var i  = 0; i< marks.length; i++){
+    console.log('hello');
+}
+
+for(var i  = 0; i< marks.length; i++){
+    var element = marks[i];
+    console.log(element);
+}
