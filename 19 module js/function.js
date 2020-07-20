62d3677a1baa72d34bd5af34974e4cbda8763ea1
Md Rana Mahmud <ranamd0@gmail.com>
2020-07-21 02:54:24 +0600

    return from funnction done

--- 19 module js/function.js
+++ 19 module js/function.js
@@ -9,6 +9,7 @@ function evenify(num) {
 }
 
 
+
 function evenify_all(nums) {
     for (let i = 0; i < nums.length; i++) {
         const num = nums[i];
@@ -62,3 +63,64 @@ for (let i = 0; i < friends_age.length; i++) {
 }
 
 evenify_all(nums);
+
+
+
+function evenify_return(num) {
+    if (num % 2 == 0) {
+        return num;
+    } else {
+        console.log(num*2, ': is odd number');
+    }
+    return 500;
+}
+var result = evenify_return(5);
+console.log(result);
+var result = evenify_return(10);
+console.log(result);
+
+
+function evenify_return2(num) {
+    if (num % 2 == 0) {
+        return num;
+    } else {
+        return num*2;
+    }
+}
+var result = evenify_return2(5);
+console.log(result);
+var result = evenify_return2(10);
+console.log(result);
+
+
+function evenify_return3(num) {
+    var result;
+    if (num % 2 == 0) {
+        result = num;
+    } else {
+        result = num*2;
+    }
+    return result;
+}
+var result = evenify_return3(5);
+console.log(result);
+var result = evenify_return3(10);
+console.log(result);
+var result = evenify_return3(13);
+
+var square = result*result;
+console.log('square', square);
+
+
+function evenify_all_arr(nums) {
+    var even_array = [];
+    for (let i = 0; i < nums.length; i++) {
+        const num = nums[i];
+       var result = evenify_return2(num);
+       even_array.push(result);
+    }
+    return even_array;
+}
+
+var nums_even = evenify_all_arr(nums);
+console.log(nums_even);
