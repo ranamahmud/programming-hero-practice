23011339b04062f1f4599fbf638726627f7287f2
Md Rana Mahmud <ranamd0@gmail.com>
2020-08-24 20:46:43 +0600

    props done

--- Module 32/core-concepts/src/App.js
+++ Module 32/core-concepts/src/App.js
@@ -1,43 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const nayoks = ['Anwar', 'Jafor', 'Alamgir', 'Salman']
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+
         <p>
-          Edit done<code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <p>
           I'm a React Person
           </p>
-         
-        </a>
-        <Person></Person>
-          <Person></Person>
-          <Person></Person>
+
+          <Person name="Munna" job="football"></Person>
+      <Person name="Massum" job="Dorshok"></Person>
       </header>
+     
+
     </div>
   );
 }
 
-function Person(){
-  const personStyle = {
-    border: '2px solid red',
-    margin: '10px'
-  }
-  return (<div style={personStyle}>
-    <h1>Name: Rana</h1>
-  <h3>Hero of the year</h3>
-  </div>)
+function Person(props){
+  return (
+    <div style={{border:'2px solid gold', width:'400px'}}>
+      <h3>My Name: {props.name}</h3>
+      <p>My Profession: {props.job}</p>
+    </div>
+  )
 }
 
 export default App;
