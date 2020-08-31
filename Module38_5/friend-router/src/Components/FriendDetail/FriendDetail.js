330ea4b1344c3e1f00bda6d5ff60f17f2833cf57
Md Rana Mahmud <ranamd0@gmail.com>
2020-09-01 01:10:55 +0600

    friend details done

--- Module38_5/friend-router/src/Components/FriendDetail/FriendDetail.js
+++ Module38_5/friend-router/src/Components/FriendDetail/FriendDetail.js
@@ -1,11 +1,22 @@
 import React from 'react';
-import {useParams} from 'react-router-dom'
+import { useParams } from 'react-router-dom';
+import { useEffect,useState } from 'react';
 const FriendDetail = () => {
-    const {friendId} = useParams();
+    const { friendId } = useParams();
+    const [friend, setFriend] = useState({});
+    useEffect(() => {
+        const url = `https://jsonplaceholder.typicode.com/users/${friendId}`;
+        fetch(url)
+            .then(res => res.json())
+            .then(data => setFriend(data))
+    })
     return (
         <div>
-            <h3>This is friend detail component</h3>
-            <p>{friendId}</p>
+            <h3>This is friend detail component {friendId}</h3>
+            <h3>{friend.name}</h3>
+            <p>email: {friend.email}</p>
+            <p>phone: {friend.phone}</p>
+            <p>website: {friend.website}</p>
         </div>
     );
 };
--- Module38_5/friend-router/src/Components/NoMatch/NoMatch.js
+++ Module38_5/friend-router/src/Components/NoMatch/NoMatch.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 const NoMatch = () => {
+    
     return (
         <div>
             <h2>Route Not Found</h2>
