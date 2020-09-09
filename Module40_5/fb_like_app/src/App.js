00920edf288bc03f9298291b419a9945501a2241
Md Rana Mahmud <ranamd0@gmail.com>
2020-09-09 10:46:02 +0600

    Load multiple data done

--- Module40_5/fb_like_app/src/App.js
+++ Module40_5/fb_like_app/src/App.js
@@ -3,26 +3,39 @@ import logo from './logo.svg';
 import './App.css';
 import ThumbUpAltIcon from '@material-ui/icons/ThumbUpAlt';
 import { useState } from 'react';
+import { useEffect } from 'react';
 function App() {
   const [likeColor, setLikeColor] = useState('');
+  const [users, setUsers] = useState([])
+  const [singleUser, setSingleUser] = useState({})
+  const [randomUser, setRandomUser] = useState({})
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users/')
+      .then(res => res.json())
+      .then(data => setUsers(data))
+
+      fetch('https://jsonplaceholder.typicode.com/users/1')
+      .then(res => res.json())
+      .then(data => setSingleUser(data))
+
+      fetch('https://randomuser.me/api/')
+      .then(res => res.json())
+      .then(data => setRandomUser(data.results[0]))
+  },[])
+
 
   return (
     <div className="App">
       <ThumbUpAltIcon onClick={() => setLikeColor(likeColor ? '' : 'primary')} color={likeColor} />
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{singleUser.name}</h1>
+      <h2>Random User Name: {randomUser.name?.first}</h2>
+      <h2>Random Gender: {randomUser.gender}</h2>
+      {
+        users.map(user =>
+          <li>{user.name}</li>
+        )
+      }
     </div>
   );
 }
