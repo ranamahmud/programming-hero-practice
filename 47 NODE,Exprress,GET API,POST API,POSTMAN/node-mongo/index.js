d3db851645adb6a20d438a2b51498d7ff3dfae5e
Md Rana Mahmud <ranamd0@gmail.com>
2020-09-27 16:54:42 +0600

    express body parser done

--- 47 NODE,Exprress,GET API,POST API,POSTMAN/node-mongo/index.js
+++ 47 NODE,Exprress,GET API,POST API,POSTMAN/node-mongo/index.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const cors = require('cors');
+var bodyParser = require('body-parser')
 
 const app = express();
 app.use(cors());
+// parse application/json
+app.use(bodyParser.json());
+
 const users = ["Asad", "Moin", "Sabed", "Susmita", "Sohana", "Shabana"]
 app.get('/', (req, res) => {
     const fruit = {
@@ -12,14 +16,20 @@ app.get('/', (req, res) => {
     res.send(fruit);
 })
 
-app.get('/fruits/banana', (req, res) => {
-    res.send({ fruit: 'banana', quantity: 1000, price: 10000 });
-})
+// app.get('/fruits/banana', (req, res) => {
+//     res.send({ fruit: 'banana', quantity: 1000, price: 10000 });
+// })
 
 app.get('/users/:id', (req, res) => {
     const id = req.params.id;
     console.log(req.query)
     const name = users[id];
     res.send({ id, name });
+})
+
+
+// post
+app.post('/addUser', (req, res) => {
+    console.log(req.body)
 })
 app.listen(4200, () => console.log("Listening to port 4200"))
