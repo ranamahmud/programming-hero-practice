2fa4c45292e8128b66ddc98b7eb10a19928ca850
Md Rana Mahmud <ranamd0@gmail.com>
2020-09-30 18:49:20 +0600

    addBooking api done

--- 49 SECURE A NODE API USING JWT TOKEN/burj-al-arab-after-auth-master/src/components/Book/Book.js
+++ 49 SECURE A NODE API USING JWT TOKEN/burj-al-arab-after-auth-master/src/components/Book/Book.js
@@ -35,6 +35,17 @@ const Book = () => {
     };
 
     const handleBooking = () => {
+        const newBooking = { ...loggedInUser, selectedDate }
+
+        fetch('http://localhost:5000/addBooking', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(newBooking)
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+            })
 
     }
 
--- 49 SECURE A NODE API USING JWT TOKEN/burj-al-arab-server/index.js
+++ 49 SECURE A NODE API USING JWT TOKEN/burj-al-arab-server/index.js
@@ -1,5 +1,11 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser');
+
+const cors = require('cors')
+
+app.use(cors())
+app.use(bodyParser.json())
 const port = 5000
 const pass = "ArabianHorse79";
 app.get('/', (req, res) => {
@@ -12,10 +18,19 @@ const MongoClient = require('mongodb').MongoClient;
 const uri = "mongodb+srv://arabian:ArabianHorse79@cluster0.jbp81.mongodb.net/burjAlArab>?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
-    const collection = client.db("test").collection("booking");
+    const bookings = client.db("burjAlArab").collection("bookings");
     console.log('db connected successfully')
     // perform actions on the collection object
-    client.close();
+    app.post('/addBooking', (req, res) => {
+        const newBooking = req.body;
+        bookings.insertOne(newBooking)
+            .then(result => {
+                res.send(result.insertedCount > 0)
+                console.log(result)
+            })
+        console.log(newBooking)
+    })
+    // client.close();
 });
 
 
