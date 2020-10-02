b8221072b65baa6d3ff9323de4ff6047d500f81f
Md Rana Mahmud <ranamd0@gmail.com>
2020-10-02 16:44:18 +0600

    load single product done

--- 50 E-COMMERCE SITE NODE AND MONGODB INTEGRATION/ema-john-with-auth/src/components/ProductDetail/ProductDetail.js
+++ 50 E-COMMERCE SITE NODE AND MONGODB INTEGRATION/ema-john-with-auth/src/components/ProductDetail/ProductDetail.js
@@ -1,12 +1,17 @@
 import React from 'react';
+import { useState } from 'react';
+import { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
-import fakeData from '../../fakeData';
 import Product from '../Product/Product';
 
 const ProductDetail = () => {
-    const {productKey} = useParams();
-    const product = fakeData.find(pd => pd.key === productKey);
-    
+    const { productKey } = useParams();
+    const [product, setProduct] = useState({})
+    useEffect(() => {
+        fetch('http://localhost:5000/product/' + productKey)
+            .then(res => res.json())
+            .then(data => setProduct(data))
+    }, [productKey])
     return (
         <div>
             <h1>Your Product Details.</h1>
