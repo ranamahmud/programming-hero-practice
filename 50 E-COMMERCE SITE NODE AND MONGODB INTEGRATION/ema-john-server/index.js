ce62360f5ab31c9499b67626e2c6278c6088dcd0
Md Rana Mahmud <ranamd0@gmail.com>
2020-10-03 17:40:13 +0600

    server review api added

--- 50 E-COMMERCE SITE NODE AND MONGODB INTEGRATION/ema-john-server/index.js
+++ 50 E-COMMERCE SITE NODE AND MONGODB INTEGRATION/ema-john-server/index.js
@@ -12,14 +12,51 @@ const MongoClient = require('mongodb').MongoClient;
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jbp81.mongodb.net/${process.env.DB_NAME}>?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
-    const products = client.db(process.env.DB_NAME).collection("products");
+    const productsCollection = client.db(process.env.DB_NAME).collection("products");
+    const ordersCollection = client.db(process.env.DB_NAME).collection("orders");
     // perform actions on the collection object
 
     app.post('/addProduct', (req, res) => {
-        const product = req.body
-        products.insertOneJ(product)
+        const products = req.body
+        productsCollection.insertOne(products)
             .then(result => {
-                console.log(result)
+                console.log(result.insertedCount)
+                res.send(result.insertedCount)
+            })
+
+    })
+
+    app.get('/products', (req, res) => {
+        productsCollection.find({})
+            .toArray((err, documents) => {
+                res.send(documents)
+            })
+    })
+
+
+    app.get('/product/:key', (req, res) => {
+        productsCollection.find({ key: req.params.key })
+            .toArray((err, documents) => {
+                res.send(documents[0])
+            })
+    })
+
+    app.post('/productByKeys', (req, res) => {
+        const productKeys = req.body;
+
+        productsCollection.find({ key: { $in: productKeys } })
+            .toArray((err, documents) => {
+                console.log(documents)
+                res.send(documents)
+            })
+    })
+
+
+    app.post('/addOrder', (req, res) => {
+        const order = req.body
+        ordersCollection.insertOne(order)
+            .then(result => {
+                res.send(result.insertedCount > 0)
             })
 
     })
