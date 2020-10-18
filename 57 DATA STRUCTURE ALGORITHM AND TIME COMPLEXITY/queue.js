e50fe3c4a62aacbfc4747c98accee89e8e528f2a
Md Rana Mahmud <ranamd0@gmail.com>
2020-10-19 05:27:01 +0600

    queue done

---
+++ 57 DATA STRUCTURE ALGORITHM AND TIME COMPLEXITY/queue.js
@@ -0,0 +1,27 @@
+class Queue {
+    constructor() {
+        this.queue = [];
+    }
+
+    enqueue(item) {
+        this.queue.push(item);
+    }
+    dequeue() {
+        if (this.queue.length) {
+            return this.queue.shift();
+        }
+    }
+}
+
+
+const premiks = new Queue();
+premiks.enqueue('Ashik')
+premiks.enqueue('Dewan')
+premiks.enqueue('Prem')
+premiks.enqueue('Priya')
+console.log(premiks.queue)
+
+
+const winner = premiks.dequeue();
+console.log(winner);
+console.log(premiks.queue)
---
+++ 57 DATA STRUCTURE ALGORITHM AND TIME COMPLEXITY/stack.js
@@ -0,0 +1,36 @@
+const friends = [];
+friends.push(45)
+friends.push(85)
+friends.push(123)
+console.log(friends)
+
+const getOne = friends.pop()
+console.log(getOne);
+console.log(friends)
+
+
+class Stack {
+    constructor() {
+        this.stack = [];
+    }
+
+    add(item) {
+        this.stack.push(item)
+    }
+    remove() {
+        if (this.stack.length) {
+            return this.stack.pop();
+        }
+    }
+}
+
+const guest = new Stack();
+guest.add("Rahman");
+guest.add("Zaman");
+guest.add("Kaman")
+guest.add("Kamran")
+console.log(guest.stack)
+
+const speaker = guest.remove();
+console.log(speaker);
+console.log(guest.stack)
