78932912b798fdea0ab479964b302a93f6869b5a
Md Rana Mahmud <ranamd0@gmail.com>
2020-11-05 11:28:46 +0600

    inch to feed done

---
+++ 14 APPLY JAVASCRIPT CONCEPTS/2.inch_to_feet.js
@@ -0,0 +1,15 @@
+var inch = 156;
+var feet = inch / 12;
+console.log(feet);
+
+function inchToFeet(inch) {
+    var feet = inch / 12;
+    return feet;
+}
+
+var nanaFeet = inchToFeet(156)
+console.log(nanaFeet)
+var naniFeet = inchToFeet(288);
+console.log(naniFeet)
+var dadiFeet = inchToFeet(300);
+console.log(dadiFeet);
